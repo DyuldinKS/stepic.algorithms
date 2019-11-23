d9dd32f0b6a47d7e7fef488a78d37323f1bdafe8
dyuldinks <dyuldin_kirill@protonmail.com>
2019-11-23 19:18:55 +0300

    fix 6.4 - reduce complexity to O(nlog(n))

--- 6.4/1.py
+++ 6.4/1.py
@@ -1,14 +1,46 @@
+def merge_counting_inversions(a, b):
+    i, j = 0, 0
+    merged = []
+    inversions = 0
+
+    while i < len(a) and j < len(b):
+        if a[i] <= b[j]:
+            merged.append(a[i])
+            i += 1
+        else:
+            merged.append(b[j])
+            j += 1
+            inversions += len(a) - i
+
+    merged.extend(a[i::] if i < len(a) else b[j::])
+    return merged, inversions
 
 
-# input()
-xs = list(map(int, input().split(' ')))
+def count_inversions(xs):
+    inversions = 0
 
-i = 0
-entropy = 0
-for x in xs:
-    for j in range(i + 1, len(xs)):
-        if xs[i] > xs[j]:
-            entropy = entropy + 1
-    i += 1
+    def merge_sort(ls):
+        if (len(ls) == 1):
+            return ls
 
-print(entropy)
+        m = len(ls) // 2
+        left = merge_sort(ls[:m:])
+        right = merge_sort(ls[m::])
+        merged, invs = merge_counting_inversions(left, right)
+
+        nonlocal inversions
+        inversions += invs
+        return merged
+
+    merge_sort(xs)
+    return inversions
+
+
+def main():
+    _ = input()
+    xs = list(map(int, input().split(' ')))
+    print(count_inversions(xs))
+
+
+if __name__ == "__main__":
+    main()
